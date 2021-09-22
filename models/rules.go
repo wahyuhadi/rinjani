61a94a382ed4803cece2bd429f33ff5eef27b411
wahyuhadi <hrahmatwahyu@gmail.com>
2021-09-22 09:53:31 +0800

    change : model struct in Rules

--- models/rules.go
+++ models/rules.go
@@ -2,18 +2,22 @@ package models
 
 type Rules struct {
 	ID   string `yaml:"id"`
-	Info struct {
-		Name        string `yaml:"name"`
-		Author      string `yaml:"author"`
-		Severity    string `yaml:"severity"`
-		Details     string `yaml:"details"`
-		Impact      string `yaml:"impact"`
-		Remediation string `yaml:"remediation"`
-	} `yaml:"info"`
+	Info Info   `yaml:"info"`
 	File struct {
-		Indexwith []struct {
-			Type  string   `yaml:"type"`
-			Regex []string `yaml:"regex"`
-		} `yaml:"indexwith"`
+		Indexwith Indexwith `yaml:"indexwith"`
 	} `yaml:"file"`
+}
+
+type Info struct {
+	Name        string `yaml:"name"`
+	Author      string `yaml:"author"`
+	Severity    string `yaml:"severity"`
+	Details     string `yaml:"details"`
+	Impact      string `yaml:"impact"`
+	Remediation string `yaml:"remediation"`
+}
+
+type Indexwith []struct {
+	Type  string   `yaml:"type"`
+	Regex []string `yaml:"regex"`
 }
